2ffc7e94c6fdb7eb420a39e93e3a320dc66ef7d8
LepkoQQ <lepko.san@gmail.com>
2021-06-03 12:29:54 +0200

    wagtail start

---
+++ obljubadeladolg/obljubadeladolg/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-^hzddfr1x23strr65$-&$+4bx3551*o#vv3a1_p$!)cvq@oz1c'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
